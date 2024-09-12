e4a09c9f5367e7291c99445c7c59f5ec432aed79
chim Yakov Barzesky <chaimyakovbar@gmail.com>
2024-09-12 14:23:16 +0300

    contact mongo atlas

--- db/mongo.js
+++ db/mongo.js
@@ -1,46 +1,43 @@
 const { MongoClient } = require("mongodb");
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env')})
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') }); // טוען את משתני הסביבה
 
-const uri = process.env.DB
-const DBName = process.env.DBNAME
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-
+// הגדרת מחרוזת החיבור ל-MongoDB עם משתני סביבה
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const internal = {
   db: null,
-}
+};
 
+// פונקציה שמתחברת למסד הנתונים
 const connectToDB = async () => {
-  let conn
   try {
-    conn = await client.connect();
-
-    console.log('Connecting to database:', uri);
-    const db = conn.db(DBName);
-    console.log('Connected to database:', DBName);
-    
-    internal.db = db
-    return db
-  } catch (e) {
-    console.error(e);
+    await client.connect();
+    console.log('Connected to database:', process.env.DB_NAME);
+    internal.db = client.db(process.env.DB_NAME); // שומר את החיבור למסד הנתונים
+    return internal.db;
+  } catch (error) {
+    console.error('Database connection error:', error);
   }
-}
-const getDB = () => internal.db
+};
 
-const getCollictionOpirtion = (collection) => {
+// פונקציה לקבלת מסד הנתונים
+const getDB = () => {
+  return internal.db;
+};
 
-}
-
+// פונקציה לקבלת אוסף (collection) ממסד הנתונים
 const getDbWithCollection = (collection) => {
-  if(internal.db){
-  return internal.db.collection(collection)
-}
-
-}
+  if (internal.db) {
+    return internal.db.collection(collection); // מחזיר אוסף מהמסד נתונים
+  } else {
+    throw new Error('Database is not connected');
+  }
+};
 
 module.exports = {
   connectToDB,
   getDB,
-  getDbWithCollection
-}
+  getDbWithCollection,
+};
